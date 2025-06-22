b8d75086e1b26e4f5709a2da91db6d1847b6619e
habybheart1 <akintobiabibat@yahoo.com>
2025-06-22 17:04:21 +0100

    navigation bar icon added

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-
+const menu = document.getElementById("nav-menu");
+const toggleButton = document.querySelector(".menu-toggle");
+const toggleIcon = toggleButton.querySelector("i");
+const menuItem = document.querySelectorAll("#nav-menu a");
+function toggleMenu() {
+  menu.classList.toggle("show");
+  const expanded = menu.classList.contains("show");
+  toggleIcon.classList.toggle("fa-times", expanded);
+  toggleIcon.className = expanded ? "fas fa-times" : "fas fa-bars";
+}
